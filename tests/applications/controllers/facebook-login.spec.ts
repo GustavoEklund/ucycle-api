fda58924a6cb9bb6a93d1b6d93ced741a8ba0ff9
Gustavo Eklund <gustavo04616@gmail.com>
2021-09-16 12:11:58 -0300

    refactor: change FacebookLoginController tests to mock RequiredStringValidator instead of test in integration

--- tests/applications/controllers/facebook-login.spec.ts
+++ tests/applications/controllers/facebook-login.spec.ts
@@ -1,15 +1,22 @@
 import { FacebookAuthentication } from '@/domain/features'
-import { mock, MockProxy } from 'jest-mock-extended'
 import { AuthenticationError } from '@/domain/errors'
 import { AccessToken } from '@/domain/models'
 import { FacebookLoginController } from '@/application/controllers'
-import { RequiredFieldError, ServerError, UnauthorizedError } from '@/application/errors'
+import { ServerError, UnauthorizedError } from '@/application/errors'
+import { RequiredStringValidator } from '@/application/validation'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+import { mocked } from 'ts-jest/utils'
+
+jest.mock('@/application/validation/required-string')
 
 describe('FacebookLoginController', () => {
   let sut: FacebookLoginController
   let facebookAuth: MockProxy<FacebookAuthentication>
+  let token: string
 
   beforeAll(() => {
+    token = 'any_token'
     facebookAuth = mock()
     facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
   })
@@ -18,43 +25,32 @@ describe('FacebookLoginController', () => {
     sut = new FacebookLoginController(facebookAuth)
   })
 
-  it('should return 400 if token is empty', async () => {
-    const httpResponse = await sut.handle({ token: '' })
+  it('should return 400 if validation fails', async () => {
+    const error = new Error('validation_error')
+    const RequiredStringValidatorSpy = jest.fn().mockImplementationOnce(() => ({
+      validate: jest.fn().mockReturnValueOnce(error)
+    }))
+    mocked(RequiredStringValidator).mockImplementationOnce(RequiredStringValidatorSpy)
 
+    const httpResponse = await sut.handle({ token })
+
+    expect(RequiredStringValidatorSpy).toHaveBeenCalledWith('any_token', 'token')
     expect(httpResponse).toEqual({
       statusCode: 400,
-      data: new RequiredFieldError('token')
-    })
-  })
-
-  it('should return 400 if token is null', async () => {
-    const httpResponse = await sut.handle({ token: null as any })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new RequiredFieldError('token')
-    })
-  })
-
-  it('should return 400 if token is undefined', async () => {
-    const httpResponse = await sut.handle({ token: undefined as any })
-
-    expect(httpResponse).toEqual({
-      statusCode: 400,
-      data: new RequiredFieldError('token')
+      data: error
     })
   })
 
   it('should call FacebookAuthentication with correct params', async () => {
-    await sut.handle({ token: 'any_token' })
+    await sut.handle({ token })
 
-    expect(facebookAuth.perform).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(facebookAuth.perform).toHaveBeenCalledWith({ token })
     expect(facebookAuth.perform).toHaveBeenCalledTimes(1)
   })
 
   it('should return 401 authentication fails', async () => {
     facebookAuth.perform.mockResolvedValueOnce(new AuthenticationError())
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
 
     expect(httpResponse).toEqual({
       statusCode: 401,
@@ -63,7 +59,7 @@ describe('FacebookLoginController', () => {
   })
 
   it('should return 200 authentication succeeds', async () => {
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
 
     expect(httpResponse).toEqual({
       statusCode: 200,
@@ -76,7 +72,7 @@ describe('FacebookLoginController', () => {
   it('should return 500 authentication throws', async () => {
     const error = new Error('infra_error')
     facebookAuth.perform.mockRejectedValueOnce(error)
-    const httpResponse = await sut.handle({ token: 'any_token' })
+    const httpResponse = await sut.handle({ token })
 
     expect(httpResponse).toEqual({
       statusCode: 500,
