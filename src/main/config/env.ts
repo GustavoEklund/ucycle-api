6be3b098596bb8d7239fa7a09ae78144ae23ad76
Gustavo Eklund <gustavo04616@gmail.com>
2021-09-16 16:49:22 -0300

    feat: ensure FacebookApi return undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '1054199952077180',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '4ade5889320b6dba1d1341b736343e62'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
-    expect(1).toBe(1)
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAOZByg2qcXwBAPzKo8sWIiPNnTtdMfyMjb7dQvJKlUQZBgkZCAGtI4ZBlEBb3vFxQwKPZAsZBkurSdpGYe0906joQayx9eLrcjWirTWTsxmaqnig7xttMZAGpPhRF0wc8tTX0da4IrGG3bSSxgn6miF5fZCA7qZAdTYVgSNuLsSzNZAeej7rJD469kUhgZCx9lxBZBwcswhZCVSSIjCEVAtoivab' })
+
+    expect(fbUser).toEqual({
+      facebookId: '102784078829237',
+      email: 'gustavo_hlmdyje_teste@tfbnw.net',
+      name: 'Gustavo Teste'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
