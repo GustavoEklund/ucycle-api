3d36d81d61abbf2f2e6cf3f29d582dbd04d743e9
Gustavo Eklund <gustavo04616@gmail.com>
2021-09-17 19:37:51 -0300

    refactor: transform ExpressRouter to a function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,16 +1,12 @@
 import { Controller } from '@/application/controllers'
 
-import { Request, Response } from 'express'
+import { Request, RequestHandler, Response } from 'express'
 
-export class ExpressRouter {
-  constructor (private readonly controller: Controller) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body })
-    if (httpResponse.statusCode === 200) {
-      res.status(200).json(httpResponse.data)
-    } else {
-      res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
-    }
+export const adaptExpressRoute = (controller: Controller): RequestHandler => async (req: Request, res: Response) => {
+  const httpResponse = await controller.handle({ ...req.body })
+  if (httpResponse.statusCode === 200) {
+    res.status(200).json(httpResponse.data)
+  } else {
+    res.status(httpResponse.statusCode).json({ error: httpResponse.data.message })
   }
 }
