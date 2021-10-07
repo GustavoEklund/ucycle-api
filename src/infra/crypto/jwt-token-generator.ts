c63dcd1c066f1bc7d969aa7c9dd993f87730e81c
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-06 22:16:50 -0300

    refactor: change Params to Input and Result to Output

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -1,13 +1,13 @@
 import { TokenGenerator } from '@/domain/contracts/crypto'
 import { sign } from 'jsonwebtoken'
 
-type Params = TokenGenerator.Params
-type Result = TokenGenerator.Result
+type Input = TokenGenerator.Params
+type Output = TokenGenerator.Result
 
 export class JwtTokenGenerator implements TokenGenerator {
   constructor (private readonly secret: string) {}
 
-  async generateToken ({ expirationInMs, key }: Params): Promise<Result> {
+  async generateToken ({ expirationInMs, key }: Input): Promise<Output> {
     const expirationInSeconds = expirationInMs / 1000
     return sign({ key }, this.secret, { expiresIn: expirationInSeconds })
   }
