65c4d6e34c539518db0d0b2294be129927c94d3e
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-10 15:32:19 -0300

    feat: add .env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,8 @@
 export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '1054199952077180',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '4ade5889320b6dba1d1341b736343e62'
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '4ade5889320b6dba1d1341b736343e62',
+    accessToken: process.env.FB_ACCESS_TOKEN ?? ''
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'w389eh3rbhu9nfy823349-rinmfi9w389w'
