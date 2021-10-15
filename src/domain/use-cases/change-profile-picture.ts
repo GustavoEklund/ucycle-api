4a0e84ef97406189f34a534b296b5ebe1b2620ef
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-14 21:06:08 -0300

    feat: ensure ChangeProfilePicture calls DeleteFile if file exists and SaveUserPicture throws

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -1,9 +1,9 @@
-import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
+import { DeleteFile, UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { LoadUserProfile, SaveUserPicture } from '@/domain/contracts/repos'
 import { UserProfile } from '@/domain/entities'
 
 type Setup = (
-  fileStorage: UploadFile,
+  fileStorage: UploadFile & DeleteFile,
   crypto: UUIDGenerator,
   userProfileRepo: SaveUserPicture & LoadUserProfile
 ) => ChangeProfilePicture
@@ -19,6 +19,10 @@ export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfil
   }
   const userProfile = new UserProfile(id)
   userProfile.setPicture(profileData)
-  await userProfileRepo.savePicture(userProfile)
+  try {
+    await userProfileRepo.savePicture(userProfile)
+  } catch {
+    await fileStorage.delete({ key })
+  }
   return userProfile
 }
