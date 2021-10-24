490943b447369711994a9203d4f0a29aec1a3878
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-24 19:21:34 -0300

    fix: ensure UserProfileRepo.load to be undefined also

--- src/domain/contracts/repos/user-profile.ts
+++ src/domain/contracts/repos/user-profile.ts
@@ -16,5 +16,5 @@ export interface LoadUserProfile {
 
 export namespace LoadUserProfile {
   export type Input = { id: string }
-  export type Output = { name?: string }
+  export type Output = { name?: string } | undefined
 }
