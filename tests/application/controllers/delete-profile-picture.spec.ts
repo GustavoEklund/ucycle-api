84032ab740bbed6c0829889ab59755d95a359348
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-25 00:08:56 -0300

    feat: ensure DeleteProfilePictureController returns 204

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,12 +1,14 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
+import { HttpResponse, noContent } from '@/application/helpers'
 
 type HttpRequest = { userId: string }
 
 class DeleteProfilePictureController {
   constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-  async handle ({ userId }: HttpRequest): Promise<void> {
+  async handle ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ id: userId })
+    return noContent()
   }
 }
 
@@ -22,10 +24,19 @@ describe('DeleteProfilePictureController', () => {
     sut = new DeleteProfilePictureController(changeProfilePicture)
   })
 
-  it('Should call ChangeProfilePicture with correct input', async () => {
+  it('should call ChangeProfilePicture with correct input', async () => {
     await sut.handle({ userId: 'any_user_id' })
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id' })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 204', async () => {
+    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
   })
 })
