7e73bdf77f3bf647351a281640e06d2c3d0dc9cc
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-28 10:10:41 -0300

    test: add AwsS3FileStorage integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,17 @@
+import { config as loadDotEnv } from 'dotenv'
+
+loadDotEnv()
+
 export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '1054199952077180',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '4ade5889320b6dba1d1341b736343e62',
     accessToken: process.env.FB_ACCESS_TOKEN ?? ''
+  },
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY ?? '',
+    secret: process.env.AWS_S3_SECRET ?? '',
+    bucket: process.env.AWS_S3_BUCKET ?? ''
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'w389eh3rbhu9nfy823349-rinmfi9w389w'
