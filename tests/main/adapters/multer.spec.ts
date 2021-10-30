303846193b2851dee549c2e6f194b6cd5ab1011f
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-30 20:28:17 -0300

    feat: ensure MulterAdapter call single upload with correct input

---
+++ tests/main/adapters/multer.spec.ts
@@ -0,0 +1,48 @@
+import { NextFunction, RequestHandler, Request, Response } from 'express'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import multer from 'multer'
+import { mocked } from 'ts-jest/utils'
+
+jest.mock('multer')
+
+const adaptMulter: RequestHandler = (req, res, next) => {
+  const upload = multer().single('picture')
+  upload(req, res, () => {})
+}
+
+describe('MulterAdapter', () => {
+  let uploadSpy: jest.Mock
+  let singleSpy: jest.Mock
+  let multerSpy: jest.Mock
+  let fakeMulter: jest.Mocked<typeof multer>
+  let req: Request
+  let res: Response
+  let next: NextFunction
+  let sut: RequestHandler
+
+  beforeAll(() => {
+    uploadSpy = jest.fn()
+    singleSpy = jest.fn().mockImplementation(() => uploadSpy)
+    multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }))
+    fakeMulter = multer as jest.Mocked<typeof multer>
+    mocked(fakeMulter).mockImplementation(multerSpy)
+    req = getMockReq()
+    res = getMockRes().res
+    next = getMockRes().next
+  })
+
+  beforeEach(() => {
+    sut = adaptMulter
+  })
+
+  it('should call single upload with correct input', () => {
+    sut(req, res, next)
+
+    expect(multerSpy).toHaveBeenCalledWith()
+    expect(multerSpy).toHaveBeenCalledTimes(1)
+    expect(singleSpy).toHaveBeenCalledWith('picture')
+    expect(singleSpy).toHaveBeenCalledTimes(1)
+    expect(uploadSpy).toHaveBeenCalledWith(req, res, expect.any(Function))
+    expect(uploadSpy).toHaveBeenCalledTimes(1)
+  })
+})
