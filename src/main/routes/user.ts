94ea96e7efe9398b0e44e54c5083f3510b73a398
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-31 14:51:11 -0300

    test: ensure PUT /users/profile/picture returns 200 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 import { auth } from '@/main/middlewares'
-import { adaptExpressRoute as adapt } from '@/main/adapters'
+import { adaptExpressRoute as adapt, adaptMulter as upload } from '@/main/adapters'
 import { makeSaveProfilePictureController } from '@/main/factories/application/controllers'
 
 export default (router: Router): void => {
-  router.put('/users/profile/picture', auth)
+  router.put('/users/profile/picture', auth, upload, adapt(makeSaveProfilePictureController()))
   router.delete('/users/profile/picture', auth, adapt(makeSaveProfilePictureController()))
 }
