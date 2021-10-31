47a2a4d69e6f25fb0a691fd963d5b4bc865d9c7a
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-31 16:01:39 -0300

    fix: connection problems

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,8 +4,8 @@ loadDotEnv()
 
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '1054199952077180',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '4ade5889320b6dba1d1341b736343e62',
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '',
     accessToken: process.env.FB_ACCESS_TOKEN ?? ''
   },
   s3: {
@@ -14,5 +14,5 @@ export const env = {
     bucket: process.env.AWS_S3_BUCKET ?? ''
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? 'w389eh3rbhu9nfy823349-rinmfi9w389w'
+  jwtSecret: process.env.JWT_SECRET ?? ''
 }
