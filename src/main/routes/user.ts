ce8d30da616420a14fc5cda2f102fe624ddc1cd3
Gustavo Eklund <gustavo04616@gmail.com>
2021-10-31 14:42:29 -0300

    feat: ensure PUT /users/profile/picture return 403 if no authorization header is present

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -4,5 +4,6 @@ import { adaptExpressRoute as adapt } from '@/main/adapters'
 import { makeSaveProfilePictureController } from '@/main/factories/application/controllers'
 
 export default (router: Router): void => {
+  router.put('/users/profile/picture', auth)
   router.delete('/users/profile/picture', auth, adapt(makeSaveProfilePictureController()))
 }
