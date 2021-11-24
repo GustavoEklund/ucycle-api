2275b6a29a011fe14348cf22fa05920021055806
Gustavo Eklund <gustavo04616@gmail.com>
2021-11-24 02:20:19 -0300

    refactor: move production code to its own files

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,36 +1,8 @@
-import { mock, MockProxy } from 'jest-mock-extended'
+import { DbTransaction } from '@/application/contracts'
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '@/application/helpers'
+import { DbTransactionControllerDecorator } from '@/application/decorators'
 
-class DbTransactionControllerDecorator extends Controller {
-  constructor (
-    private readonly decoratee: Controller,
-    private readonly db: DbTransaction
-  ) {
-    super()
-  }
-
-  async perform (httpRequest: any): Promise<HttpResponse> {
-    await this.db.openTransaction()
-    try {
-      const httpResponse = await this.decoratee.perform(httpRequest)
-      await this.db.commit()
-      return httpResponse
-    } catch (error) {
-      await this.db.rollback()
-      throw error
-    } finally {
-      await this.db.closeTransaction()
-    }
-  }
-}
-
-interface DbTransaction {
-  openTransaction: () => Promise<void>
-  closeTransaction: () => Promise<void>
-  commit: () => Promise<void>
-  rollback: () => Promise<void>
-}
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('DbTransactionControllerDecorator', () => {
   let db: MockProxy<DbTransaction>
