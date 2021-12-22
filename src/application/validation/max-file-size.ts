3db782b29998d430cae66f88378cfb6e96cb36f2
Gustavo Eklund <gustavo04616@gmail.com>
2021-12-22 19:32:31 -0300

    fix: hardcoded value

--- src/application/validation/max-file-size.ts
+++ src/application/validation/max-file-size.ts
@@ -5,7 +5,7 @@ export class MaxFileSize implements Validator {
   constructor(private readonly maxSizeInMb: number, private readonly value: Buffer) {}
 
   validate(): Error | undefined {
-    const maxFileSizeInBytes = 5 * 1024 * 1024
+    const maxFileSizeInBytes = this.maxSizeInMb * 1024 * 1024
     if (this.value.length > maxFileSizeInBytes) return new MaxFileSizeError(this.maxSizeInMb)
   }
 }
