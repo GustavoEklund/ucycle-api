48b624f8f287cbbe5af09eb098479b46ae7ea59b
DeElfos <santosefilhomarcos@gmail.com>
2022-02-22 21:22:03 -0300

    chore: automate multiple database creation

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,7 +2,15 @@
 
 if [ ! -f ".env" ]; then
     cp .env.sample .env
+    echo "[ERROR] .env file not found. Created .env file with sample values. Please edit .env file and restart container."
+    exit 0
+fi
+
+if [ "$(cat .env)" = "$(cat .env.sample)" ]; then
+    echo "[ERROR] .env file is with sample values. Please edit .env file and restart container."
+    exit 0
 fi
 
 npm install
+typeorm migration:run
 npm run dev
