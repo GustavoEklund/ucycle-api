24288d8c1ae7e8b73ed2f746921c78b9dc2aec74
Gustavo Eklund <gustavo04616@gmail.com>
2022-04-28 12:32:45 -0300

    feat: ensure LoadMyOrganizationsController return 200 with a list of organizations on success

--- src/application/controllers/organizations/load-my-organizations.ts
+++ src/application/controllers/organizations/load-my-organizations.ts
@@ -1,4 +1,5 @@
 import { LoadMyOrganizations } from '@/domain/use-cases'
+import { HttpResponse, ok } from '@/application/helpers'
 
 type HttpRequest = {
   userId: string
@@ -7,7 +8,8 @@ type HttpRequest = {
 export class LoadMyOrganizationsController {
   public constructor(private readonly loadMyOrganizations: LoadMyOrganizations) {}
 
-  public async handle({ userId }: HttpRequest): Promise<void> {
-    await this.loadMyOrganizations.perform({ userId })
+  public async handle({ userId }: HttpRequest): Promise<HttpResponse<LoadMyOrganizations.Output>> {
+    const organizations = await this.loadMyOrganizations.perform({ userId })
+    return ok(organizations)
   }
 }
