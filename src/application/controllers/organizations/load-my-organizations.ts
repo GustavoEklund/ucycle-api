4c411509c3bdf650c088bf7a4d232240ae63e40f
Gustavo Eklund <gustavo04616@gmail.com>
2022-04-28 13:03:23 -0300

    feat: ensure LoadMyOrganizationsController build validators correctly

--- src/application/controllers/organizations/load-my-organizations.ts
+++ src/application/controllers/organizations/load-my-organizations.ts
@@ -1,6 +1,7 @@
 import { LoadMyOrganizations } from '@/domain/use-cases'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Controller } from '@/application/controllers'
+import { RequiredType, ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 type HttpRequest = {
   userId: string
@@ -14,5 +15,9 @@ export class LoadMyOrganizationsController extends Controller {
   public async perform({ userId }: HttpRequest): Promise<HttpResponse<LoadMyOrganizations.Output>> {
     const organizations = await this.loadMyOrganizations.perform({ userId })
     return ok(organizations)
+  }
+
+  public override buildValidators({ userId }: any): Validator[] {
+    return Builder.of({ fieldName: 'userId', value: userId }).required(RequiredType.string).build()
   }
 }
