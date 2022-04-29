f8e19c8aa533ca4ebf893fd6e787001ac6f7f87a
Gustavo Eklund <gustavo04616@gmail.com>
2022-04-29 11:18:04 -0300

    feat: ensure LoadMyOrganizationsUseCase return the output of LoadOrganizations

--- tests/domain/use-cases/organization/load-my-organizations.spec.ts
+++ tests/domain/use-cases/organization/load-my-organizations.spec.ts
@@ -11,6 +11,25 @@ describe('LoadMyOrganizationsUseCase', () => {
   beforeAll(() => {
     userId = 'userId'
     organizationRepoSpy = mock()
+    organizationRepoSpy.loadAll.mockResolvedValue([
+      {
+        id: 'any_organization_id',
+        name: 'any_organization_name',
+        address: {
+          city: 'any_city',
+          state: 'any_state',
+          country: 'any_country',
+          street: 'any_street',
+          neighbourhood: 'any_neighbourhood',
+          buildingNumber: 72,
+        },
+        pictures: [
+          {
+            url: 'any_url',
+          },
+        ],
+      },
+    ])
   })
 
   beforeEach(() => {
@@ -21,5 +40,29 @@ describe('LoadMyOrganizationsUseCase', () => {
     await sut.perform({ userId })
 
     expect(organizationRepoSpy.loadAll).toHaveBeenCalledWith({ userId })
+  })
+
+  it('should return the output of LoadOrganizations', async () => {
+    const output = await sut.perform({ userId })
+
+    expect(output).toEqual([
+      {
+        id: 'any_organization_id',
+        name: 'any_organization_name',
+        address: {
+          city: 'any_city',
+          state: 'any_state',
+          country: 'any_country',
+          street: 'any_street',
+          neighbourhood: 'any_neighbourhood',
+          buildingNumber: 72,
+        },
+        pictures: [
+          {
+            url: 'any_url',
+          },
+        ],
+      },
+    ])
   })
 })
