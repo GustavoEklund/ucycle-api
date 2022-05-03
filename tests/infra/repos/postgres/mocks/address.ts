179fbbefec541660e723099edff22cbf806230d6
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-03 17:24:53 -0300

    test: ensure PgOrganizationRepository load all organizations

---
+++ tests/infra/repos/postgres/mocks/address.ts
@@ -0,0 +1,9 @@
+export const mockAddress = () => ({
+  city: 'any_city',
+  state: 'any_state',
+  country: 'any_country',
+  street: 'any_street',
+  neighbourhood: 'any_neighbourhood',
+  postalCode: 'any_postal_code',
+  buildingNumber: 76,
+})
