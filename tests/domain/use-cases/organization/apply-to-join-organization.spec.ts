2b1b079fef47f1bd28a596a24df2dcaaf37f1557
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-04 16:35:21 -0300

    test: ensure ApplyToJoinOrganizationUseCace throw UserAccountNotFoundError if user account not found

--- tests/domain/use-cases/organization/apply-to-join-organization.spec.ts
+++ tests/domain/use-cases/organization/apply-to-join-organization.spec.ts
@@ -3,6 +3,7 @@ import {
   ApplyToJoinOrganizationUseCase,
 } from '@/domain/use-cases/organizations'
 import { LoadUserAccount } from '@/domain/contracts/repos'
+import { UserAccountNotFoundError } from '@/domain/entities/errors'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
@@ -12,6 +13,10 @@ describe('ApplyToJoinOrganizationUseCace', () => {
 
   beforeAll(() => {
     userAccountRepoSpy = mock()
+    userAccountRepoSpy.load.mockResolvedValue({
+      id: 'any_user_id',
+      name: 'any_user_name',
+    })
   })
 
   beforeEach(() => {
@@ -23,5 +28,13 @@ describe('ApplyToJoinOrganizationUseCace', () => {
 
     expect(userAccountRepoSpy.load).toHaveBeenCalledTimes(1)
     expect(userAccountRepoSpy.load).toHaveBeenCalledWith({ id: 'any_user_id' })
+  })
+
+  it('should throw UserAccountNotFoundError if user account not found', async () => {
+    userAccountRepoSpy.load.mockResolvedValue(undefined)
+
+    const promise = sut.perform({ userId: 'any_user_id', organizationId: 'any_organization_id' })
+
+    await expect(promise).rejects.toThrowError(new UserAccountNotFoundError('any_user_id'))
   })
 })
