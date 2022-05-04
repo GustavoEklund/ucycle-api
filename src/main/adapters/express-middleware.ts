a599d374a2887e8c0974f6f9a0181d282303c146
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-04 11:26:00 -0300

    refactor: make express route and middleware receive query, params and headers on request

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -4,7 +4,12 @@ import { RequestHandler } from 'express'
 type Adapter = (middleware: Middleware) => RequestHandler
 
 export const adaptExpressMiddleware: Adapter = (middleware) => async (req, res, next) => {
-  const { statusCode, data } = await middleware.handle({ ...req.headers })
+  const { statusCode, data } = await middleware.handle({
+    ...req.query,
+    ...req.params,
+    ...req.body,
+    ...req.headers,
+  })
   if (statusCode === 200) {
     const entries = Object.entries(data).filter((entry) => entry[1])
     req.locals = { ...req.locals, ...Object.fromEntries(entries) }
