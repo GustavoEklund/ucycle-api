fc79edaaaa0cc912ba11b3b9389b080deb78dd7c
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-06 14:53:26 -0300

    feat: ensure ApplyToJoinOrganizationController extend Controller

--- src/application/controllers/organizations/apply-to-join-organization.ts
+++ src/application/controllers/organizations/apply-to-join-organization.ts
@@ -1,14 +1,22 @@
 import { ApplyToJoinOrganization } from '@/domain/use-cases'
+import { Controller } from '@/application/controllers'
+import { HttpResponse, ok } from '@/application/helpers'
 
 type HttpRequest = {
   userId: string
   organizationId: string
 }
 
-export class ApplyToJoinOrganizationController {
-  public constructor(private readonly applyToJoinOrganization: ApplyToJoinOrganization) {}
+export class ApplyToJoinOrganizationController extends Controller {
+  public constructor(private readonly applyToJoinOrganization: ApplyToJoinOrganization) {
+    super()
+  }
 
-  public async handle({ userId, organizationId }: HttpRequest): Promise<void> {
+  public override async perform({
+    userId,
+    organizationId,
+  }: HttpRequest): Promise<HttpResponse<string>> {
     await this.applyToJoinOrganization.perform({ userId, organizationId })
+    return ok('')
   }
 }
