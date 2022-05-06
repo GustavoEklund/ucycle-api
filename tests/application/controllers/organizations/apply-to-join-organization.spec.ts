ad957b8ab256f4213554af71e24cac36f9e0d324
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-06 16:01:36 -0300

    test: ensure ApplyToJoinOrganizationController build validators correctly

--- tests/application/controllers/organizations/apply-to-join-organization.spec.ts
+++ tests/application/controllers/organizations/apply-to-join-organization.spec.ts
@@ -2,6 +2,7 @@ import { ApplyToJoinOrganizationController, Controller } from '@/application/con
 import { ApplyToJoinOrganization } from '@/domain/use-cases'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { RequiredString } from '@/application/validation'
 
 describe('ApplyToJoinOrganizationController', () => {
   let sut: ApplyToJoinOrganizationController
@@ -19,13 +20,27 @@ describe('ApplyToJoinOrganizationController', () => {
     expect(sut).toBeInstanceOf(Controller)
   })
 
+  it('should build validators correctly', () => {
+    const expectedValidators = [
+      new RequiredString('any_user_id', 'userId'),
+      new RequiredString('any_organization_id', 'organizationId'),
+    ]
+
+    const validators = sut.buildValidators({
+      userId: 'any_user_id',
+      organizationId: 'any_organization_id',
+    })
+
+    expect(validators).toEqual(expectedValidators)
+  })
+
   it('should call ApplyToJoinOrganization with correct input', async () => {
-    await sut.handle({ userId: 'userId', organizationId: 'organizationId' })
+    await sut.handle({ userId: 'any_user_id', organizationId: 'any_organization_id' })
 
     expect(applyToJoinOrganizationSpy.perform).toHaveBeenCalledTimes(1)
     expect(applyToJoinOrganizationSpy.perform).toHaveBeenCalledWith({
-      userId: 'userId',
-      organizationId: 'organizationId',
+      userId: 'any_user_id',
+      organizationId: 'any_organization_id',
     })
   })
 })
