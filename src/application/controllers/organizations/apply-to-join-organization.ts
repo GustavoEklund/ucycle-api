a1d3c9de5d59127ef7b151e51afaa66922404259
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-06 16:00:51 -0300

    feat: ensure ApplyToJoinOrganizationController build validators correctly

--- src/application/controllers/organizations/apply-to-join-organization.ts
+++ src/application/controllers/organizations/apply-to-join-organization.ts
@@ -1,6 +1,7 @@
 import { ApplyToJoinOrganization } from '@/domain/use-cases'
 import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
+import { RequiredType, ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 type HttpRequest = {
   userId: string
@@ -18,5 +19,14 @@ export class ApplyToJoinOrganizationController extends Controller {
   }: HttpRequest): Promise<HttpResponse<string>> {
     await this.applyToJoinOrganization.perform({ userId, organizationId })
     return ok('')
+  }
+
+  public override buildValidators({ userId, organizationId }: HttpRequest): Validator[] {
+    return [
+      ...Builder.of({ value: userId, fieldName: 'userId' }).required(RequiredType.string).build(),
+      ...Builder.of({ value: organizationId, fieldName: 'organizationId' })
+        .required(RequiredType.string)
+        .build(),
+    ]
   }
 }
