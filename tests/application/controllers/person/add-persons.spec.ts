26daee418fa52038f876859919cef25d74e4dd7d
DeElfos <santosefilhomarcos@gmail.com>
2022-05-19 04:40:19 -0300

    test: add person/controller - optimize testing code

--- tests/application/controllers/person/add-persons.spec.ts
+++ tests/application/controllers/person/add-persons.spec.ts
@@ -1,30 +1,24 @@
 import { AddPersonsController, Controller } from '@/application/controllers'
 import { RequiredInteger, RequiredString } from '@/application/validation'
 
+const personsData = {
+  firstName: 'any_first_name',
+  lastName: 'any_last_name',
+
+  birthDate: '14/05/1999',
+  professional: 'any_professional',
+  marriedStatus: 'any_married_status',
+
+  specialNeeds: false,
+  specialNeedsDescription: 'any_special_needs_description',
+}
+
 describe('AddPersonsController', () => {
-  let firstName: string
-  let lastName: string
-
-  let birthDate: string
-  let professional: string
-  let marriedStatus: string
-
-  let specialNeeds: boolean
-  let specialNeedsDescription: string
-
   let sut: AddPersonsController
   let addPersonsSpy: jest.Mock
 
   beforeAll(() => {
-    firstName: 'any_first_name'
-    lastName: 'any_last_name'
-
-    birthDate: '14/05/1999'
-    professional: 'any_professional'
-    marriedStatus: 'any_married_status'
-
-    specialNeeds: false
-    specialNeedsDescription: 'any_special_needs_description'
+    personsData
 
     addPersonsSpy = jest.fn()
     addPersonsSpy.mockResolvedValue({ id: 'any_id' })
@@ -40,36 +34,18 @@ describe('AddPersonsController', () => {
 
   it('should call AddPersons with correct input', async () => {
     await sut.handle({
-      firstName,
-      lastName,
-      birthDate,
-      professional,
-      marriedStatus,
-      specialNeeds,
-      specialNeedsDescription,
+      ...personsData,
     })
 
     expect(addPersonsSpy).toHaveBeenCalledWith({
-      firstName,
-      lastName,
-      birthDate,
-      professional,
-      marriedStatus,
-      specialNeeds,
-      specialNeedsDescription,
+      ...personsData,
     })
     expect(addPersonsSpy).toHaveBeenCalledTimes(1)
   })
 
   it('should return 200 authentication succeeds', async () => {
     const httpResponse = await sut.handle({
-      firstName,
-      lastName,
-      birthDate,
-      professional,
-      marriedStatus,
-      specialNeeds,
-      specialNeedsDescription,
+      ...personsData,
     })
 
     expect(httpResponse).toEqual({
