888f0b8ca2fb06846983f97046480cfaf5e0cd79
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-19 14:51:38 -0300

    test: ensure Document to be a valid cnpj document

--- tests/domain/value-objects/document.spec.ts
+++ tests/domain/value-objects/document.spec.ts
@@ -7,4 +7,11 @@ describe('Document', () => {
     expect(sut.type).toBe(DocumentType.cpf)
     expect(sut.number).toBe('34244419888')
   })
+
+  it('should be a valid cnpj document', () => {
+    const sut = new Document('32.063.364/0001-07')
+
+    expect(sut.type).toBe(DocumentType.cnpj)
+    expect(sut.number).toBe('32063364000107')
+  })
 })
