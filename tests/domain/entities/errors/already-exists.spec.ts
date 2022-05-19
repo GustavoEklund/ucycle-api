6540796008b8dbe9b3a62a01980c4652e0344d7e
DeElfos <santosefilhomarcos@gmail.com>
2022-05-19 05:21:37 -0300

    test: domain/already-exists - should return correct message

--- tests/domain/entities/errors/already-exists.spec.ts
+++ tests/domain/entities/errors/already-exists.spec.ts
@@ -1,13 +1,17 @@
 import { AlreadyExistsError } from '@/domain/entities/errors'
 
 describe('AlreadyExistsError', () => {
-    let sut: AlreadyExistsError
+  let sut: AlreadyExistsError
 
   beforeEach(() => {
-    sut = new AlreadyExistsError('any_entity', 'any_id')
+    sut = new AlreadyExistsError('any_id', 'any_entity')
   })
 
   it('should extend Error', () => {
     expect(sut).toBeInstanceOf(Error)
+  })
+
+  it('should return correct message', () => {
+    expect(sut.message).toBe('any_entity with id any_id already exists')
   })
 })
