b2ec411bfe50613c77cbf2c7786984c24c32e4d9
Gustavo Eklund <gustavo04616@gmail.com>
2022-05-19 14:23:20 -0300

    test: ensure HttpHelpers.unauthorized be a 401 UnauthorizedError

--- tests/application/helpers/http.spec.ts
+++ tests/application/helpers/http.spec.ts
@@ -1,9 +1,20 @@
-import { notFound } from '@/application/helpers'
-import { NotFoundError } from '@/application/errors'
+import { notFound, unauthorized } from '@/application/helpers'
+import { NotFoundError, UnauthorizedError } from '@/application/errors'
 
 describe('HttpHelpers', () => {
-  describe('notFound', () => {
-    it('should return generic NotFoundError if error is undefined', () => {
+  describe('401 unauthorized', () => {
+    it('should be a 401 UnauthorizedError', () => {
+      const sut = unauthorized()
+
+      expect(sut).toEqual({
+        statusCode: 401,
+        data: new UnauthorizedError(),
+      })
+    })
+  })
+
+  describe('404 notFound', () => {
+    it('should be generic NotFoundError if error is undefined', () => {
       const expectedError = new NotFoundError()
 
       const sut = notFound()
@@ -11,7 +22,7 @@ describe('HttpHelpers', () => {
       expect(sut.data).toEqual(expectedError)
     })
 
-    it('should return correct error when provided', () => {
+    it('should be correct error when provided', () => {
       const expectedError = new Error('any_error')
 
       const sut = notFound(expectedError)
