b892d982bb7d1db162b1d90f2fc4b54a64c637b1
DeElfos <santosefilhomarcos@gmail.com>
2022-05-20 11:56:52 -0300

    test: domain/use-cases/ass-persons - should call personsRepo.save with correct values

---
+++ tests/domain/use-cases/persons/add-persons.spec.ts
@@ -0,0 +1,47 @@
+import { AddPersons, setupAddPersons } from '@/domain/use-cases/persons/add-persons'
+import { Persons } from '@/domain/entities/persons/persons'
+import { mockPerson } from '../../../infra/repos/postgres/mocks/person'
+import { SavePersons } from '@/domain/contracts/repos/persons/persons'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+let generatePerson = () => {
+  const persons = new Persons(mockPerson())
+
+  return {
+    firstName: persons.firstName,
+    lastName: persons.lastName,
+
+    // document: persons.document,
+    // contact: persons.contact,
+
+    birthDate: persons.birthDate,
+    professional: persons.professional,
+    marriedStatus: persons.marriedStatus,
+
+    specialNeeds: persons.specialNeeds,
+    specialNeedsDescription: persons.specialNeedsDescription,
+  }
+}
+
+jest.mock('@/domain/entities/persons/persons')
+
+describe('use-cases add-persons', () => {
+  let sut: AddPersons
+  let mockAddPersonsContract: MockProxy<SavePersons>
+
+  beforeAll(() => {
+    mockAddPersonsContract = mock()
+    mockAddPersonsContract.save.mockResolvedValue({ id: '1' })
+  })
+
+  beforeEach(() => {
+    sut = setupAddPersons(mockAddPersonsContract)
+  })
+
+  it('should call personsRepo.save with correct values', async () => {
+    const person = generatePerson()
+    await sut(person)
+
+    expect(mockAddPersonsContract.save).toHaveBeenCalledWith(person)
+  })
+})
