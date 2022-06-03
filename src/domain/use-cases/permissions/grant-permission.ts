5aaecf5bf218ffc1502816ce4d1e2eee1ec1ce10
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-03 18:38:53 -0300

    feat: ensure GrantPermissionUseCase throw UserAccountNotFoundError if LoadUserAccount returns undefined

--- src/domain/use-cases/permissions/grant-permission.ts
+++ src/domain/use-cases/permissions/grant-permission.ts
@@ -1,4 +1,5 @@
 import { LoadUserAccount } from '@/domain/contracts/repos'
+import { UserAccountNotFoundError } from '@/domain/entities/errors'
 
 export interface GrantPermission {
   perform: (input: GrantPermission.Input) => Promise<GrantPermission.Output>
@@ -8,7 +9,8 @@ export class GrantPermissionUseCase implements GrantPermission {
   public constructor(private readonly userRepo: LoadUserAccount) {}
 
   public async perform({ userId }: GrantPermission.Input): Promise<GrantPermission.Output> {
-    await this.userRepo.load({ id: userId })
+    const user = await this.userRepo.load({ id: userId })
+    if (user === undefined) throw new UserAccountNotFoundError(userId)
   }
 }
 
