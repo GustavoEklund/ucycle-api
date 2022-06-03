913ee12216f36f0e2012a19c32e36d252dfd9caf
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-03 18:39:27 -0300

    test: ensure GrantPermissionUseCase throw UserAccountNotFoundError if LoadUserAccount returns undefined

--- tests/domain/use-cases/permissions/grant-permission.spec.ts
+++ tests/domain/use-cases/permissions/grant-permission.spec.ts
@@ -1,14 +1,38 @@
 import { LoadUserAccount } from '@/domain/contracts/repos'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { GrantPermissionUseCase } from '@/domain/use-cases/permissions'
+import { UserAccountNotFoundError } from '@/domain/entities/errors'
 
 describe('GrantPermissionUseCase', () => {
-  it('should call LoadUserAccount with correct input', async () => {
-    const userRepo = mock<LoadUserAccount>()
-    const sut = new GrantPermissionUseCase(userRepo)
+  let userRepo: MockProxy<LoadUserAccount>
+  let sut: GrantPermissionUseCase
 
+  beforeAll(() => {
+    userRepo = mock()
+    userRepo.load.mockResolvedValue({
+      id: 'any_user_id',
+      lastName: 'any_last_name',
+      firstName: 'any_first_name',
+      contacts: [],
+      documents: [],
+    })
+  })
+
+  beforeEach(() => {
+    sut = new GrantPermissionUseCase(userRepo)
+  })
+
+  it('should call LoadUserAccount with correct input', async () => {
     await sut.perform({ userId: 'any_user_id' })
 
     expect(userRepo.load).toHaveBeenCalledWith({ id: 'any_user_id' })
+  })
+
+  it('should throw UserAccountNotFoundError if LoadUserAccount returns undefined', async () => {
+    userRepo.load.mockResolvedValueOnce(undefined)
+
+    const promise = sut.perform({ userId: 'any_user_id' })
+
+    await expect(promise).rejects.toThrow(new UserAccountNotFoundError('any_user_id'))
   })
 })
