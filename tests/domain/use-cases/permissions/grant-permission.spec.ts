adb02847449aed75c1b36e245cc0a121405527c6
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-06 17:00:45 -0300

    test: ensure GrantPermissionUseCase call SavePermission with correct input

--- tests/domain/use-cases/permissions/grant-permission.spec.ts
+++ tests/domain/use-cases/permissions/grant-permission.spec.ts
@@ -1,50 +1,66 @@
-import { LoadUserAccount } from '@/domain/contracts/repos'
+import { LoadUserAccount, SavePermission } from '@/domain/contracts/repos'
 import { mock, MockProxy } from 'jest-mock-extended'
-import { GrantPermissionUseCase } from '@/domain/use-cases/permissions'
+import { GrantPermission, GrantPermissionUseCase } from '@/domain/use-cases/permissions'
 import { UserAccountNotFoundError } from '@/domain/entities/errors'
 
 describe('GrantPermissionUseCase', () => {
-  let userRepo: MockProxy<LoadUserAccount>
+  let userRepoSpy: MockProxy<LoadUserAccount>
+  let permissionRepoSpy: MockProxy<SavePermission>
+  let grantPermissionInput: GrantPermission.Input
   let sut: GrantPermissionUseCase
 
   beforeAll(() => {
-    userRepo = mock()
-    userRepo.load.mockResolvedValue({
+    grantPermissionInput = {
+      grantById: 'any_user_id',
+      grantToId: 'any_target_user_id',
+      resourceCode: 'any_resource_code',
+      resourceName: 'any_resource_name',
+      description: 'any_resource_description',
+      write: true,
+      read: true,
+      owner: false,
+      status: 'GRANTED',
+      moduleId: 'any_module_id',
+      resourceId: 'any_resource_id',
+    }
+    userRepoSpy = mock()
+    userRepoSpy.load.mockResolvedValue({
       id: 'any_user_id',
       lastName: 'any_last_name',
       firstName: 'any_first_name',
       contacts: [],
       documents: [],
     })
+    permissionRepoSpy = mock()
   })
 
   beforeEach(() => {
-    sut = new GrantPermissionUseCase(userRepo)
+    sut = new GrantPermissionUseCase(userRepoSpy, permissionRepoSpy)
   })
 
   it('should call LoadUserAccount with correct input', async () => {
-    await sut.perform({ userId: 'any_user_id', targetUserId: 'any_target_user_id' })
+    await sut.perform(grantPermissionInput)
 
-    expect(userRepo.load.mock.calls[0][0]).toEqual({ id: 'any_user_id' })
+    expect(userRepoSpy.load.mock.calls[0][0]).toEqual({ id: 'any_user_id' })
   })
 
   it('should return UserAccountNotFoundError if LoadUserAccount returns undefined', async () => {
-    userRepo.load.mockResolvedValueOnce(undefined)
+    userRepoSpy.load.mockResolvedValueOnce(undefined)
 
-    const output = await sut.perform({ userId: 'any_user_id', targetUserId: 'any_target_user_id' })
+    const output = await sut.perform(grantPermissionInput)
 
     expect(output).toEqual(new UserAccountNotFoundError('any_user_id'))
   })
 
   it('should call LoadUserAccount with correct input for target user', async () => {
-    await sut.perform({ userId: 'any_user_id', targetUserId: 'any_target_user_id' })
+    await sut.perform(grantPermissionInput)
 
-    expect(userRepo.load.mock.calls[1][0]).toEqual({ id: 'any_target_user_id' })
-    expect(userRepo.load).toHaveBeenCalledTimes(2)
+    expect(userRepoSpy.load.mock.calls[1][0]).toEqual({ id: 'any_target_user_id' })
+    expect(userRepoSpy.load).toHaveBeenCalledTimes(2)
   })
 
   it('should return UserAccountNotFoundError if LoadUserAccount returns undefined for target user', async () => {
-    userRepo.load
+    userRepoSpy.load
       .mockResolvedValueOnce({
         id: 'any_user_id',
         lastName: 'any_last_name',
@@ -54,8 +70,27 @@ describe('GrantPermissionUseCase', () => {
       })
       .mockResolvedValueOnce(undefined)
 
-    const output = await sut.perform({ userId: 'any_user_id', targetUserId: 'any_target_user_id' })
+    const output = await sut.perform(grantPermissionInput)
 
     expect(output).toEqual(new UserAccountNotFoundError('any_target_user_id'))
+  })
+
+  it('should call SavePermission with correct input', async () => {
+    await sut.perform(grantPermissionInput)
+
+    expect(permissionRepoSpy.save).toHaveBeenCalledTimes(1)
+    expect(permissionRepoSpy.save).toHaveBeenCalledWith({
+      grantById: 'any_user_id',
+      grantToId: 'any_target_user_id',
+      resourceCode: 'any_resource_code',
+      resourceName: 'any_resource_name',
+      description: 'any_resource_description',
+      read: true,
+      write: true,
+      owner: false,
+      status: 'GRANTED',
+      moduleId: 'any_module_id',
+      resourceId: 'any_resource_id',
+    })
   })
 })
