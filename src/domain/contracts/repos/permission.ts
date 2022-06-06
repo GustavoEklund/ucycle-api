d5e6f205e41cffa8702844567269c0b7f106e5bc
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-06 16:53:26 -0300

    feat: ensure GrantPermissionUseCase call SavePermission with correct input

---
+++ src/domain/contracts/repos/permission.ts
@@ -0,0 +1,22 @@
+export interface SavePermission {
+  save: (input: SavePermission.Input) => Promise<SavePermission.Output>
+}
+
+export namespace SavePermission {
+  export type Input = {
+    grantById: string
+    grantToId: string
+    resourceCode: string
+    resourceName: string
+    description: string
+    read: boolean
+    write: boolean
+    owner: boolean
+    status: string
+    moduleId: string
+    resourceId: string
+  }
+  export type Output = {
+    id: string
+  }
+}
--- src/domain/use-cases/permissions/grant-permission.ts
+++ src/domain/use-cases/permissions/grant-permission.ts
@@ -1,4 +1,4 @@
-import { LoadUserAccount } from '@/domain/contracts/repos'
+import { LoadUserAccount, SavePermission } from '@/domain/contracts/repos'
 import { UserAccountNotFoundError } from '@/domain/entities/errors'
 
 export interface GrantPermission {
@@ -6,23 +6,33 @@ export interface GrantPermission {
 }
 
 export class GrantPermissionUseCase implements GrantPermission {
-  public constructor(private readonly userRepo: LoadUserAccount) {}
+  public constructor(
+    private readonly userRepo: LoadUserAccount,
+    private readonly permissionRepo: SavePermission
+  ) {}
 
-  public async perform({
-    userId,
-    targetUserId,
-  }: GrantPermission.Input): Promise<GrantPermission.Output> {
-    const user = await this.userRepo.load({ id: userId })
-    if (user === undefined) return new UserAccountNotFoundError(userId)
-    const targetUser = await this.userRepo.load({ id: targetUserId })
-    if (targetUser === undefined) return new UserAccountNotFoundError(targetUserId)
+  public async perform(input: GrantPermission.Input): Promise<GrantPermission.Output> {
+    const user = await this.userRepo.load({ id: input.grantById })
+    if (user === undefined) return new UserAccountNotFoundError(input.grantById)
+    const targetUser = await this.userRepo.load({ id: input.grantToId })
+    if (targetUser === undefined) return new UserAccountNotFoundError(input.grantToId)
+    await this.permissionRepo.save(input)
   }
 }
 
 export namespace GrantPermission {
   export type Input = {
-    userId: string
-    targetUserId: string
+    grantById: string
+    grantToId: string
+    resourceCode: string
+    resourceName: string
+    description: string
+    read: boolean
+    write: boolean
+    owner: boolean
+    status: string
+    moduleId: string
+    resourceId: string
   }
   export type Output = undefined | UserAccountNotFoundError
 }
