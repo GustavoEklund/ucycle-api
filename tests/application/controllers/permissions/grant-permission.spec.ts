81a76215b96085d1cd8591ecbcf74255794aa0cd
vitor <vitorhprsilva@gmail.com>
2022-06-21 18:13:39 -0300

    test: ensure RequiredBoolean build validators correctly

--- tests/application/controllers/permissions/grant-permission.spec.ts
+++ tests/application/controllers/permissions/grant-permission.spec.ts
@@ -1,13 +1,15 @@
-import { GrantPermissionController } from '@/application/controllers/permissions'
+import { GrantPermissionController, HttpRequest } from '@/application/controllers/permissions'
+import { RequiredBoolean, RequiredString } from '@/application/validation'
 import { GrantPermission } from '@/domain/use-cases/permissions'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('GrantPermissionController', () => {
-  it('should call GrantPermission with correct input', async () => {
-    const grantPermissionSpy = mock<GrantPermission>()
-    const sut = new GrantPermissionController(grantPermissionSpy)
-
-    await sut.perform({
+  let grantPermissionSpy: MockProxy<GrantPermission>
+  let sut: GrantPermissionController
+  let httpRequestStub: HttpRequest
+  beforeAll(() => {
+    grantPermissionSpy = mock()
+    httpRequestStub = {
       grantById: 'any_user_id',
       grantToId: 'any_grant_to_id',
       code: 'any_code',
@@ -17,7 +19,13 @@ describe('GrantPermissionController', () => {
       status: 'any_status',
       moduleId: 'any_module_id',
       resourceId: 'any_resource_id',
-    })
+    }
+  })
+  beforeEach(() => {
+    sut = new GrantPermissionController(grantPermissionSpy)
+  })
+  it('should call GrantPermission with correct input', async () => {
+    await sut.perform(httpRequestStub)
 
     expect(grantPermissionSpy.perform).toHaveBeenCalledTimes(1)
     expect(grantPermissionSpy.perform).toHaveBeenCalledWith({
@@ -31,5 +39,23 @@ describe('GrantPermissionController', () => {
       moduleId: 'any_module_id',
       resourceId: 'any_resource_id',
     })
+  })
+
+  it('should build validators correctly', () => {
+    const expectedValidators = [
+      new RequiredString('any_user_id', 'grantById'),
+      new RequiredString('any_grant_to_id', 'grantToId'),
+      new RequiredString('any_code', 'code'),
+      new RequiredBoolean(false, 'read'),
+      new RequiredBoolean(false, 'write'),
+      new RequiredBoolean(false, 'owner'),
+      new RequiredString('any_status', 'status'),
+      new RequiredString('any_module_id', 'moduleId'),
+      new RequiredString('any_resource_id', 'resourceId'),
+    ]
+
+    const validators = sut.buildValidators(httpRequestStub)
+
+    expect(validators).toEqual(expectedValidators)
   })
 })
