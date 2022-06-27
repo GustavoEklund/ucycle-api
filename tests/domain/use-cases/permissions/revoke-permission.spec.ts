59e8a9e0c7871b286be374c5a1c70dbfc7b6c656
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-27 16:48:20 -0300

    test: ensure RevokePermissionUseCase return UserNotFoundError if LoadUserRepository returns undefined

--- tests/domain/use-cases/permissions/revoke-permission.spec.ts
+++ tests/domain/use-cases/permissions/revoke-permission.spec.ts
@@ -1,6 +1,7 @@
 import { mock } from 'jest-mock-extended'
 import { LoadUserAccount } from '@/domain/contracts/repos'
 import { RevokePermissionUseCase } from '@/domain/use-cases/permissions'
+import { UserNotFoundError } from '@/domain/entities/errors'
 
 describe('RevokePermissionUseCase', () => {
   it('should call LoadUserRepository with correct input', async () => {
@@ -16,5 +17,20 @@ describe('RevokePermissionUseCase', () => {
     })
 
     expect(loadUserRepositorySpy.load).toHaveBeenCalledWith({ id: 'any_user_id' })
+  })
+
+  it('should return UserNotFoundError if LoadUserRepository returns undefined', async () => {
+    const loadUserRepositorySpy = mock<LoadUserAccount>()
+    const sut = new RevokePermissionUseCase(loadUserRepositorySpy)
+
+    const output = await sut.perform({
+      user: { id: 'any_user_id' },
+      targetUser: {
+        id: 'any_target_user_id',
+        permission: { id: 'any_permission_id' },
+      },
+    })
+
+    expect(output).toEqual(new UserNotFoundError('any_user_id'))
   })
 })
