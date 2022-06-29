14699e34e3364f2401a6d5c242fcb297dd30d4ac
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-29 17:31:31 -0300

    feat: ensure RevokePermissionController build validators correctly

--- src/application/controllers/permissions/revoke-permission.ts
+++ src/application/controllers/permissions/revoke-permission.ts
@@ -1,9 +1,10 @@
 import { RevokePermission } from '@/domain/use-cases/permissions'
 import { Controller } from '@/application/controllers'
 import { HttpResponse, ok } from '@/application/helpers'
+import { RequiredType, ValidationBuilder, Validator } from '@/application/validation'
 
-type HttpRequest = {
-  user: { id: string }
+export type HttpRequest = {
+  userId: string
   targetUser: {
     id: string
     permission: { id: string }
@@ -15,8 +16,25 @@ export class RevokePermissionController extends Controller {
     super()
   }
 
-  public async perform({ user, targetUser }: HttpRequest): Promise<HttpResponse<undefined>> {
-    await this.revokePermission.perform({ user, targetUser })
+  public async perform({ userId, targetUser }: HttpRequest): Promise<HttpResponse<undefined>> {
+    await this.revokePermission.perform({ user: { id: userId }, targetUser })
     return ok(undefined)
+  }
+
+  public override buildValidators({ userId, targetUser }: HttpRequest): Validator[] {
+    return [
+      ...ValidationBuilder.of({ value: userId, fieldName: 'userId' })
+        .required(RequiredType.string)
+        .build(),
+      ...ValidationBuilder.of({ value: targetUser.id, fieldName: 'targetUser.id' })
+        .required(RequiredType.string)
+        .build(),
+      ...ValidationBuilder.of({
+        value: targetUser.permission.id,
+        fieldName: 'targetUser.permission.id',
+      })
+        .required(RequiredType.string)
+        .build(),
+    ]
   }
 }
