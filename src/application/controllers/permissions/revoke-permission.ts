06e885ba81f1a37efdc2e7df53f9c2948bf73032
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-29 16:35:32 -0300

    feat: ensure RevokePermissionController extends Controller

--- src/application/controllers/permissions/revoke-permission.ts
+++ src/application/controllers/permissions/revoke-permission.ts
@@ -1,4 +1,6 @@
 import { RevokePermission } from '@/domain/use-cases/permissions'
+import { Controller } from '@/application/controllers'
+import { HttpResponse, ok } from '@/application/helpers'
 
 type HttpRequest = {
   user: { id: string }
@@ -8,10 +10,13 @@ type HttpRequest = {
   }
 }
 
-export class RevokePermissionController {
-  public constructor(private readonly revokePermission: RevokePermission) {}
+export class RevokePermissionController extends Controller {
+  public constructor(private readonly revokePermission: RevokePermission) {
+    super()
+  }
 
-  public async perform({ user, targetUser }: HttpRequest): Promise<void> {
+  public async perform({ user, targetUser }: HttpRequest): Promise<HttpResponse<undefined>> {
     await this.revokePermission.perform({ user, targetUser })
+    return ok(undefined)
   }
 }
