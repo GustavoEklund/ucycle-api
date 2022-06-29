440c3c68cb475181e0b1beef6c0ec21497451c58
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-29 15:27:10 -0300

    test: ensure RevokePermissionUseCase return UserPermissionNotFound if LoadUserPermissionRepository returns undefined

--- tests/domain/use-cases/permissions/revoke-permission.spec.ts
+++ tests/domain/use-cases/permissions/revoke-permission.spec.ts
@@ -1,6 +1,6 @@
 import { LoadUserAccount, LoadUserPermission, SaveUserPermission } from '@/domain/contracts/repos'
-import { RevokePermissionUseCase } from '@/domain/use-cases/permissions'
-import { UserNotFoundError } from '@/domain/entities/errors'
+import { RevokePermission, RevokePermissionUseCase } from '@/domain/use-cases/permissions'
+import { UserNotFoundError, UserPermissionNotFoundError } from '@/domain/entities/errors'
 import { User } from '@/domain/entities/user'
 import { UserPermission } from '@/domain/entities/permission'
 import { mockUser, mockUserPermission } from '@/tests/domain/mocks/entities'
@@ -14,8 +14,16 @@ describe('RevokePermissionUseCase', () => {
   let targetUserStub: User
   let userPermissionStub: UserPermission
   let sut: RevokePermissionUseCase
+  let inputStub: RevokePermission.Input
 
   beforeAll(() => {
+    inputStub = {
+      user: { id: 'any_user_id' },
+      targetUser: {
+        id: 'any_target_user_id',
+        permission: { id: 'any_permission_id' },
+      },
+    }
     userStub = mockUser()
     targetUserStub = mockUser()
     userPermissionStub = mockUserPermission()
@@ -30,13 +38,7 @@ describe('RevokePermissionUseCase', () => {
   })
 
   it('should call LoadUserRepository with correct input', async () => {
-    await sut.perform({
-      user: { id: 'any_user_id' },
-      targetUser: {
-        id: 'any_target_user_id',
-        permission: { id: 'any_permission_id' },
-      },
-    })
+    await sut.perform(inputStub)
 
     expect(userRepositorySpy.load).toHaveBeenNthCalledWith(1, { id: 'any_user_id' })
   })
@@ -44,25 +46,13 @@ describe('RevokePermissionUseCase', () => {
   it('should return UserNotFoundError if LoadUserRepository returns undefined', async () => {
     userRepositorySpy.load.mockResolvedValueOnce(undefined)
 
-    const output = await sut.perform({
-      user: { id: 'any_user_id' },
-      targetUser: {
-        id: 'any_target_user_id',
-        permission: { id: 'any_permission_id' },
-      },
-    })
+    const output = await sut.perform(inputStub)
 
     expect(output).toEqual(new UserNotFoundError('any_user_id'))
   })
 
   it('should call LoadUserRepository with correct input for target user', async () => {
-    await sut.perform({
-      user: { id: 'any_user_id' },
-      targetUser: {
-        id: 'any_target_user_id',
-        permission: { id: 'any_permission_id' },
-      },
-    })
+    await sut.perform(inputStub)
 
     expect(userRepositorySpy.load).toHaveBeenNthCalledWith(2, { id: 'any_target_user_id' })
   })
@@ -70,39 +60,29 @@ describe('RevokePermissionUseCase', () => {
   it('should return UserNotFoundError if LoadUserRepository returns undefined for target user', async () => {
     userRepositorySpy.load.mockResolvedValueOnce(mockUser()).mockResolvedValueOnce(undefined)
 
-    const output = await sut.perform({
-      user: { id: 'any_user_id' },
-      targetUser: {
-        id: 'any_target_user_id',
-        permission: { id: 'any_permission_id' },
-      },
-    })
+    const output = await sut.perform(inputStub)
 
     expect(output).toEqual(new UserNotFoundError('any_target_user_id'))
   })
 
   it('should call LoadUserPermissionRepository with correct input', async () => {
-    await sut.perform({
-      user: { id: 'any_user_id' },
-      targetUser: {
-        id: 'any_target_user_id',
-        permission: { id: 'any_permission_id' },
-      },
-    })
+    await sut.perform(inputStub)
 
     expect(userPermissionRepositorySpy.load).toHaveBeenCalledWith({ id: 'any_permission_id' })
+  })
+
+  it('should return UserPermissionNotFound if LoadUserPermissionRepository returns undefined', async () => {
+    userPermissionRepositorySpy.load.mockResolvedValueOnce(undefined)
+
+    const output = await sut.perform(inputStub)
+
+    expect(output).toEqual(new UserPermissionNotFoundError('any_permission_id'))
   })
 
   it('should call SaveUserPermissionRepository with correct input', async () => {
     const revokeSpy = jest.spyOn(userPermissionStub, 'revoke')
 
-    await sut.perform({
-      user: { id: 'any_user_id' },
-      targetUser: {
-        id: 'any_target_user_id',
-        permission: { id: 'any_permission_id' },
-      },
-    })
+    await sut.perform(inputStub)
 
     expect(revokeSpy).toHaveBeenCalledTimes(1)
     expect(userPermissionRepositorySpy.save).toHaveBeenCalledTimes(1)
