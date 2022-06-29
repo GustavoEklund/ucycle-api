58d4cc8c305ad73a565da483cd66cd86a3d91396
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-29 18:09:25 -0300

    feat: ensure RevokePermissionController return 404 if RevokePermission returns UserPermissionNotFound

--- src/application/controllers/permissions/revoke-permission.ts
+++ src/application/controllers/permissions/revoke-permission.ts
@@ -2,7 +2,7 @@ import { RevokePermission } from '@/domain/use-cases/permissions'
 import { Controller } from '@/application/controllers'
 import { HttpResponse, notFound, ok } from '@/application/helpers'
 import { RequiredType, ValidationBuilder, Validator } from '@/application/validation'
-import { UserNotFoundError } from '@/domain/entities/errors'
+import { UserNotFoundError, UserPermissionNotFoundError } from '@/domain/entities/errors'
 
 export type HttpRequest = {
   userId: string
@@ -23,6 +23,7 @@ export class RevokePermissionController extends Controller {
   }: HttpRequest): Promise<HttpResponse<undefined | Error[]>> {
     const output = await this.revokePermission.perform({ user: { id: userId }, targetUser })
     if (output instanceof UserNotFoundError) return notFound([output])
+    if (output instanceof UserPermissionNotFoundError) return notFound([output])
     return ok(undefined)
   }
 
