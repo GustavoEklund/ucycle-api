005bdb0023b04854dfc2dce6f4dc48d954f7d92c
Gustavo Eklund <gustavo04616@gmail.com>
2022-06-29 15:00:31 -0300

    test: ensure RevokePermissionUseCase call SaveUserPermissionRepository with correct input

--- tests/domain/use-cases/permissions/revoke-permission.spec.ts
+++ tests/domain/use-cases/permissions/revoke-permission.spec.ts
@@ -1,18 +1,28 @@
-import { mock, MockProxy } from 'jest-mock-extended'
-import { LoadUserAccount, LoadUserPermission } from '@/domain/contracts/repos'
+import { LoadUserAccount, LoadUserPermission, SaveUserPermission } from '@/domain/contracts/repos'
 import { RevokePermissionUseCase } from '@/domain/use-cases/permissions'
 import { UserNotFoundError } from '@/domain/entities/errors'
-import { mockUser } from '@/tests/domain/mocks/entities'
+import { User } from '@/domain/entities/user'
+import { UserPermission } from '@/domain/entities/permission'
+import { mockUser, mockUserPermission } from '@/tests/domain/mocks/entities'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('RevokePermissionUseCase', () => {
   let userRepositorySpy: MockProxy<LoadUserAccount>
-  let userPermissionRepositorySpy: MockProxy<LoadUserPermission>
+  let userPermissionRepositorySpy: MockProxy<LoadUserPermission & SaveUserPermission>
+  let userStub: User
+  let targetUserStub: User
+  let userPermissionStub: UserPermission
   let sut: RevokePermissionUseCase
 
   beforeAll(() => {
+    userStub = mockUser()
+    targetUserStub = mockUser()
+    userPermissionStub = mockUserPermission()
     userRepositorySpy = mock()
-    userRepositorySpy.load.mockResolvedValue(mockUser())
+    userRepositorySpy.load.mockResolvedValue(userStub).mockResolvedValue(targetUserStub)
     userPermissionRepositorySpy = mock()
+    userPermissionRepositorySpy.load.mockResolvedValue(userPermissionStub)
   })
 
   beforeEach(() => {
@@ -81,5 +91,21 @@ describe('RevokePermissionUseCase', () => {
     })
 
     expect(userPermissionRepositorySpy.load).toHaveBeenCalledWith({ id: 'any_permission_id' })
+  })
+
+  it('should call SaveUserPermissionRepository with correct input', async () => {
+    const revokeSpy = jest.spyOn(userPermissionStub, 'revoke')
+
+    await sut.perform({
+      user: { id: 'any_user_id' },
+      targetUser: {
+        id: 'any_target_user_id',
+        permission: { id: 'any_permission_id' },
+      },
+    })
+
+    expect(revokeSpy).toHaveBeenCalledTimes(1)
+    expect(userPermissionRepositorySpy.save).toHaveBeenCalledTimes(1)
+    expect(userPermissionRepositorySpy.save).toHaveBeenCalledWith(userPermissionStub)
   })
 })
