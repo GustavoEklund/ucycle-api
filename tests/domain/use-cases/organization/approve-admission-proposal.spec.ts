ff1b8444c086bc45b9c0f1f98e9da5cec60b66f2
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-01 12:05:43 -0300

    test: ensure ApproveAdmissionProposalUseCase return UserNotFoundError if LoadUserRepository returns undefined

--- tests/domain/use-cases/organization/approve-admission-proposal.spec.ts
+++ tests/domain/use-cases/organization/approve-admission-proposal.spec.ts
@@ -1,15 +1,32 @@
-import { ApproveAdmissionProposalUseCase } from '@/domain/use-cases'
+import { ApproveAdmissionProposal, ApproveAdmissionProposalUseCase } from '@/domain/use-cases'
 import { LoadUserAccount } from '@/domain/contracts/repos'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { UserNotFoundError } from '@/domain/entities/errors'
 
 describe('ApproveAdmissionProposalUseCase', () => {
+  let userRepoSpy: MockProxy<LoadUserAccount>
+  let sut: ApproveAdmissionProposal
+
+  beforeAll(() => {
+    userRepoSpy = mock()
+  })
+
+  beforeEach(() => {
+    sut = new ApproveAdmissionProposalUseCase(userRepoSpy)
+  })
+
   it('should call LoadUserRepository with correct input', async () => {
-    const userRepoSpy = mock<LoadUserAccount>()
     const sut = new ApproveAdmissionProposalUseCase(userRepoSpy)
 
     await sut.perform({ user: { id: 'any_user_id' } })
 
     expect(userRepoSpy.load).toHaveBeenCalledWith({ id: 'any_user_id' })
+  })
+
+  it('should return UserNotFoundError if LoadUserRepository returns undefined', async () => {
+    const output = await sut.perform({ user: { id: 'any_user_id' } })
+
+    expect(output).toEqual(new UserNotFoundError('any_user_id'))
   })
 })
