e809764eb3192bf1d75498c8c1c5d45182f50d9a
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-06 18:40:59 -0300

    feat: ensure JoinUserToOrganizationUseCase return UserNotFoundError if LoadUserAccount returns undefined

--- src/domain/use-cases/organizations/join-user-to-organization.ts
+++ src/domain/use-cases/organizations/join-user-to-organization.ts
@@ -1,13 +1,30 @@
 import { Observer } from '@/domain/events'
 import { AdmissionProposalAccepted } from '@/domain/events/organization'
 import { LoadUserAccount } from '@/domain/contracts/repos'
+import { UserNotFoundError } from '@/domain/entities/errors'
 
-export class JoinUserToOrganizationUseCase extends Observer {
+interface JoinUserToOrganization {
+  perform: (input: JoinUserToOrganization.Input) => Promise<JoinUserToOrganization.Output>
+}
+
+export class JoinUserToOrganizationUseCase extends Observer implements JoinUserToOrganization {
   public constructor(private readonly userRepository: LoadUserAccount) {
     super({ domainEvents: ['ADMISSION_PROPOSAL_ACCEPTED'] })
   }
 
-  public async handle(input: AdmissionProposalAccepted): Promise<void> {
+  public async handle(input: AdmissionProposalAccepted): Promise<JoinUserToOrganization.Output> {
     await this.userRepository.load({ id: input.admissionProposal.userId })
+    return new UserNotFoundError(input.acceptedByUser.id)
   }
+
+  public async perform(
+    input: JoinUserToOrganization.Input
+  ): Promise<JoinUserToOrganization.Output> {
+    return this.handle(input)
+  }
+}
+
+namespace JoinUserToOrganization {
+  export type Input = AdmissionProposalAccepted
+  export type Output = undefined | UserNotFoundError
 }
