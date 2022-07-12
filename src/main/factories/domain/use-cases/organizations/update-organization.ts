168c3a4ed8025ae2bd69904c616f6f8787bf4378
LucasJRufino <Lucas.C.Rufino@hotmail.com>
2022-07-12 20:02:52 -0300

    fix: wrong identation

--- src/main/factories/domain/use-cases/organizations/update-organization.ts
+++ src/main/factories/domain/use-cases/organizations/update-organization.ts
@@ -1,5 +1,8 @@
 import { UpdateOrganization, UpdateOrganizationUseCase } from '@/domain/use-cases'
-import { makePgUserAccountRepo, makePgOrganizationRepo } from '@/main/factories/infra/repos/postgres'
+import {
+  makePgUserAccountRepo,
+  makePgOrganizationRepo,
+} from '@/main/factories/infra/repos/postgres'
 
 export const makeUpdateOrganization = (): UpdateOrganization => {
   return new UpdateOrganizationUseCase(makePgOrganizationRepo(), makePgUserAccountRepo())
