ca789bfd3ab563ea7324f0fc28acc9f3e709560b
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-12 19:27:39 -0300

    feat: ensure DeclineAdmissionProposalUseCase return AdmissionProposalNotFoundError if LoadAdmissionProposal returns undefined

--- src/domain/use-cases/organizations/decline-admission-proposal.ts
+++ src/domain/use-cases/organizations/decline-admission-proposal.ts
@@ -1,5 +1,5 @@
 import { LoadAdmissionProposal, LoadUserAccount } from '@/domain/contracts/repos'
-import { UserNotFoundError } from '@/domain/entities/errors'
+import { AdmissionProposalNotFoundError, UserNotFoundError } from '@/domain/entities/errors'
 
 export interface DeclineAdmissionProposal {
   perform: (input: DeclineAdmissionProposal.Input) => Promise<DeclineAdmissionProposal.Output>
@@ -15,8 +15,9 @@ export class DeclineAdmissionProposalUseCase {
     input: DeclineAdmissionProposal.Input
   ): Promise<DeclineAdmissionProposal.Output> {
     const user = await this.userRepo.load({ id: input.user.id })
-    if (user === undefined) return new UserNotFoundError('any_user_id')
+    if (user === undefined) return new UserNotFoundError(input.user.id)
     await this.admissionProposalRepo.load({ id: input.admissionProposal.id })
+    return new AdmissionProposalNotFoundError('any_admission_proposal_id')
   }
 }
 
@@ -29,5 +30,5 @@ export namespace DeclineAdmissionProposal {
       id: string
     }
   }
-  export type Output = undefined | UserNotFoundError
+  export type Output = undefined | UserNotFoundError | AdmissionProposalNotFoundError
 }
