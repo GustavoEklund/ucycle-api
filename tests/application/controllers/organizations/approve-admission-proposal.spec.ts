18ac2b25cac9cb526a52b2cc6268bae4a532c59d
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-12 17:34:05 -0300

    test: ensure ApproveAdmissionProposalController return 200 if ApproveAdmissionProposal returns undefined

---
+++ tests/application/controllers/organizations/approve-admission-proposal.spec.ts
@@ -0,0 +1,86 @@
+import { ApproveAdmissionProposalController } from '@/application/controllers/organizations'
+import { ApproveAdmissionProposal } from '@/domain/use-cases'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { AdmissionProposalNotFoundError, UserNotFoundError } from '@/domain/entities/errors'
+import { notFound } from '@/application/helpers'
+import { Controller } from '@/application/controllers'
+import { RequiredString } from '@/application/validation'
+
+describe('ApproveAdmissionProposalController', () => {
+  let httpResponseStub: MockProxy<{ userId: string; admissionProposalId: string }>
+  let sut: ApproveAdmissionProposalController
+  let approveAdmissionProposalSpy: MockProxy<ApproveAdmissionProposal>
+
+  beforeAll(() => {
+    httpResponseStub = {
+      userId: 'any_user_id',
+      admissionProposalId: 'any_admission_proposal_id',
+    }
+    approveAdmissionProposalSpy = mock()
+    approveAdmissionProposalSpy.perform.mockResolvedValue(undefined)
+  })
+
+  beforeEach(() => {
+    sut = new ApproveAdmissionProposalController(approveAdmissionProposalSpy)
+  })
+
+  it('should extend Controller', () => {
+    expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should build validators correctly', () => {
+    const expectedValidators = [
+      new RequiredString('any_user_id', 'userId'),
+      new RequiredString('any_admission_proposal_id', 'admissionProposalId'),
+    ]
+
+    const validators = sut.buildValidators({
+      userId: 'any_user_id',
+      admissionProposalId: 'any_admission_proposal_id',
+    })
+
+    expect(validators).toEqual(expectedValidators)
+  })
+
+  it('should call ApproveAdmissionProposal with correct input', async () => {
+    await sut.handle(httpResponseStub)
+
+    expect(approveAdmissionProposalSpy.perform).toHaveBeenCalledTimes(1)
+    expect(approveAdmissionProposalSpy.perform).toHaveBeenCalledWith({
+      user: { id: 'any_user_id' },
+      admissionProposal: { id: 'any_admission_proposal_id' },
+    })
+  })
+
+  it('should return 404 if ApproveAdmissionProposal return AdmissionProposalNotFoundError', async () => {
+    const expectedError = new AdmissionProposalNotFoundError('any_admission_proposal_id')
+    const expectedHttpResponse = notFound([expectedError])
+    approveAdmissionProposalSpy.perform.mockResolvedValueOnce(expectedError)
+
+    const httpResponse = await sut.handle(httpResponseStub)
+
+    expect(httpResponse).toEqual(expectedHttpResponse)
+  })
+
+  it('should return 404 if ApproveAdmissionProposal returns UserNotFoundError', async () => {
+    const expectedError = new UserNotFoundError('any_user_id')
+    const expectedHttpResponse = notFound([expectedError])
+    approveAdmissionProposalSpy.perform.mockResolvedValueOnce(expectedError)
+
+    const httpResponse = await sut.handle(httpResponseStub)
+
+    expect(httpResponse).toEqual(expectedHttpResponse)
+  })
+
+  it('should return 200 if ApproveAdmissionProposal returns undefined', async () => {
+    const httpResponse = await sut.handle({
+      userId: 'any_user_id',
+      admissionProposalId: 'any_admission_proposal_id',
+    })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: undefined,
+    })
+  })
+})
