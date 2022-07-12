667b37bb1a8a767458aa46ba89f404ed9d35b764
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-12 19:08:31 -0300

    test: ensure DeclineAdmissionProposalUseCase return UserNotFoundError if LoadUserRepository returns undefined

--- tests/domain/use-cases/organization/decline-admission-proposal.spec.ts
+++ tests/domain/use-cases/organization/decline-admission-proposal.spec.ts
@@ -1,15 +1,33 @@
-import { DeclineAdmissionProposalUseCase } from '@/domain/use-cases'
-import { mock } from 'jest-mock-extended'
+import { DeclineAdmissionProposal, DeclineAdmissionProposalUseCase } from '@/domain/use-cases'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { LoadUserAccount } from '@/domain/contracts/repos'
+import { UserNotFoundError } from '@/domain/entities/errors'
 
 describe('DeclineAdmissionProposalUseCase', () => {
-  it('should call LoadUserRepository with correct input', async () => {
-    const userRepoSpy = mock<LoadUserAccount>()
-    const sut = new DeclineAdmissionProposalUseCase(userRepoSpy)
+  let userRepoSpy: MockProxy<LoadUserAccount>
+  let sut: DeclineAdmissionProposal
 
+  beforeAll(() => {
+    userRepoSpy = mock()
+  })
+
+  beforeEach(() => {
+    sut = new DeclineAdmissionProposalUseCase(userRepoSpy)
+  })
+
+  it('should call LoadUserRepository with correct input', async () => {
     await sut.perform({ user: { id: 'any_user_id' } })
 
     expect(userRepoSpy.load).toHaveBeenCalledTimes(1)
     expect(userRepoSpy.load).toHaveBeenCalledWith({ id: 'any_user_id' })
+  })
+
+  it('should return UserNotFoundError if LoadUserRepository returns undefined', async () => {
+    userRepoSpy.load.mockResolvedValueOnce(undefined)
+    const expectedError = new UserNotFoundError('any_user_id')
+
+    const output = await sut.perform({ user: { id: 'any_user_id' } })
+
+    expect(output).toEqual(expectedError)
   })
 })
