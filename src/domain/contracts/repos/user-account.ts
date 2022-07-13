54653a27fb3e346adc0cb3dc6bdf779fd133092c
vitor <vitorhprsilva@gmail.com>
2022-07-13 11:41:11 -0300

    feat: ensure PgUserRepository save user account

--- src/domain/contracts/repos/user-account.ts
+++ src/domain/contracts/repos/user-account.ts
@@ -1,28 +1,5 @@
 import { User } from '@/domain/entities/user'
 
-type Document = {
-  number: string
-  type: string
-}
-
-type Email = {
-  address: string
-  type: string
-}
-
-type Phone = {
-  number: string
-  countryCode: string
-  areaCode: string
-  type: string
-}
-
-type Contact = {
-  type: string
-  verified: boolean
-  value: Email | Phone
-}
-
 export interface LoadUserAccount {
   load: (params: LoadUserAccount.Input) => Promise<LoadUserAccount.Output>
 }
@@ -41,15 +18,6 @@ export interface SaveUserAccount {
 }
 
 export namespace SaveUserAccount {
-  export type Input = {
-    id?: string
-    firstName: string
-    lastName: string
-    firstAccess: boolean
-    documents: Document[]
-    contacts: Contact[]
-  }
-  export type Output = {
-    id: string
-  }
+  export type Input = User
+  export type Output = void
 }
--- src/infra/repos/postgres/user-account.ts
+++ src/infra/repos/postgres/user-account.ts
@@ -1,10 +1,13 @@
-import { LoadUserAccount } from '@/domain/contracts/repos/user-account'
+import { LoadUserAccount, SaveUserAccount } from '@/domain/contracts/repos/user-account'
 import { PgRepository } from './repository'
-import { PgUser } from '@/infra/repos/postgres/entities'
+import { PgContact, PgDocument, PgUser } from '@/infra/repos/postgres/entities'
 import { User, UserAccount, UserAccountStatus, UserProfile } from '@/domain/entities/user'
 import { EmailType, PhoneType } from '@/domain/value-objects/contact'
 
-export class PgUserAccountRepository extends PgRepository implements LoadUserAccount {
+export class PgUserAccountRepository
+  extends PgRepository
+  implements LoadUserAccount, SaveUserAccount
+{
   public async load({ id, email }: LoadUserAccount.Input): Promise<LoadUserAccount.Output> {
     const pgUserRepo = this.getRepository(PgUser)
     const pgUserQueryBuilder = pgUserRepo.createQueryBuilder('user')
@@ -36,5 +39,34 @@ export class PgUserAccountRepository extends PgRepository implements LoadUserAcc
         profile: userProfile,
       })
     }
+  }
+
+  public async save(user: SaveUserAccount.Input): Promise<SaveUserAccount.Output> {
+    const pgUserRepo = this.getRepository(PgUser)
+    const pgDocumentRepo = this.getRepository(PgDocument)
+    const pgContactRepo = this.getRepository(PgContact)
+    const pgUser = await pgUserRepo.save({
+      id: user.id,
+      firstName: user.account.name.first,
+      lastName: user.account.name.last,
+      pictureUrl: user.profile.pictureUrl,
+      initials: user.profile.initials,
+    })
+    await pgDocumentRepo.save(
+      user.account.documents.map((document) => ({
+        user: pgUser,
+        type: document.type,
+        number: document.number,
+      }))
+    )
+    await pgContactRepo.save(
+      user.account.contacts.map((contact) => ({
+        user: pgUser,
+        type: contact.type,
+        value: contact.value,
+        label: contact.label,
+        isPrivate: contact.isPrivate,
+      }))
+    )
   }
 }
