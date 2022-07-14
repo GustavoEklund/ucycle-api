2d2a554e079da8880c4388ca5b4d6711715f6cd9
vitor <vitorhprsilva@gmail.com>
2022-07-14 01:33:10 -0300

    feat: ensure SignUpUseCase call load contact with correct phone input

--- src/domain/use-cases/users/signup.ts
+++ src/domain/use-cases/users/signup.ts
@@ -18,8 +18,9 @@ export class SignUpUseCase extends Publisher implements SignUp {
     const cpf = account.documents[0]
     const document = await this.documentRepo.load({ number: cpf })
     if (document !== undefined) return new DocumentAlreadyExistsError(cpf)
-    await this.contactRepo.load({ value: account.emails[0] })
-    return new ContactAlreadyExistsError(account.emails[0])
+    const emailContact = await this.contactRepo.load({ value: account.emails[0] })
+    if (emailContact !== undefined) return new ContactAlreadyExistsError(account.emails[0])
+    await this.contactRepo.load({ value: account.phones[0] })
   }
 }
 
