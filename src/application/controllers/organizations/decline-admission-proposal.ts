c8d4fe4b8c8badce4e0573de4415f01c4bd5ea6e
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-21 17:21:21 -0300

    feat: ensure DeclineAdmissionProposalController return 404 if DeclineAdmissionProposal returns UserNotFoundError

--- src/application/controllers/organizations/decline-admission-proposal.ts
+++ src/application/controllers/organizations/decline-admission-proposal.ts
@@ -1,4 +1,6 @@
 import { DeclineAdmissionProposal } from '@/domain/use-cases'
+import { UserNotFoundError } from '@/domain/entities/errors'
+import { HttpResponse, notFound } from '@/application/helpers'
 
 type HttpRequest = {
   userId: string
@@ -8,10 +10,11 @@ type HttpRequest = {
 export class DeclineAdmissionProposalController {
   public constructor(private readonly declineAdmissionProposal: DeclineAdmissionProposal) {}
 
-  public async handle(input: HttpRequest): Promise<void> {
+  public async handle(input: HttpRequest): Promise<HttpResponse<undefined | Error[]>> {
     await this.declineAdmissionProposal.perform({
       user: { id: input.userId },
       admissionProposal: { id: input.admissionProposalId },
     })
+    return notFound([new UserNotFoundError(input.userId)])
   }
 }
