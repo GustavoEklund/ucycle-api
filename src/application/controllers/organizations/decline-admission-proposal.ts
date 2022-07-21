49bb100154f9aeec366ea2f6083ab4d036415502
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-21 17:44:40 -0300

    feat: ensure DeclineAdmissionProposalController return 200 if DeclineAdmissionProposal returns undefined

--- src/application/controllers/organizations/decline-admission-proposal.ts
+++ src/application/controllers/organizations/decline-admission-proposal.ts
@@ -4,7 +4,7 @@ import {
   UnauthorizedUserError,
   UserNotFoundError,
 } from '@/domain/entities/errors'
-import { forbidden, HttpResponse, notFound } from '@/application/helpers'
+import { forbidden, HttpResponse, notFound, ok } from '@/application/helpers'
 
 type HttpRequest = {
   userId: string
@@ -14,7 +14,7 @@ type HttpRequest = {
 export class DeclineAdmissionProposalController {
   public constructor(private readonly declineAdmissionProposal: DeclineAdmissionProposal) {}
 
-  public async handle(input: HttpRequest): Promise<HttpResponse<Error[]> | undefined> {
+  public async handle(input: HttpRequest): Promise<HttpResponse<undefined | Error[]>> {
     const output = await this.declineAdmissionProposal.perform({
       user: { id: input.userId },
       admissionProposal: { id: input.admissionProposalId },
@@ -22,5 +22,6 @@ export class DeclineAdmissionProposalController {
     if (output instanceof UserNotFoundError) return notFound([output])
     if (output instanceof AdmissionProposalNotFoundError) return notFound([output])
     if (output instanceof UnauthorizedUserError) return forbidden([output])
+    return ok(undefined)
   }
 }
