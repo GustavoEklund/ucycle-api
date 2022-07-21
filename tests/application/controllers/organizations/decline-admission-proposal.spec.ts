d84572ac746fc3c4c75c53db7c427babff116854
Gustavo Eklund <gustavo04616@gmail.com>
2022-07-21 17:21:57 -0300

    test: ensure DeclineAdmissionProposalController return 404 if DeclineAdmissionProposal returns UserNotFoundError

--- tests/application/controllers/organizations/decline-admission-proposal.spec.ts
+++ tests/application/controllers/organizations/decline-admission-proposal.spec.ts
@@ -1,13 +1,22 @@
 import { DeclineAdmissionProposalController } from '@/application/controllers'
 import { DeclineAdmissionProposal } from '@/domain/use-cases'
 
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { UserNotFoundError } from '@/domain/entities/errors'
 
 describe('DeclineAdmissionProposalController', () => {
-  it('should call DeclineAdmissionProposal with correct input', async () => {
-    const declineAdmissionProposalSpy = mock<DeclineAdmissionProposal>()
-    const sut = new DeclineAdmissionProposalController(declineAdmissionProposalSpy)
+  let declineAdmissionProposalSpy: MockProxy<DeclineAdmissionProposal>
+  let sut: DeclineAdmissionProposalController
 
+  beforeAll(() => {
+    declineAdmissionProposalSpy = mock()
+  })
+
+  beforeEach(() => {
+    sut = new DeclineAdmissionProposalController(declineAdmissionProposalSpy)
+  })
+
+  it('should call DeclineAdmissionProposal with correct input', async () => {
     await sut.handle({
       userId: 'any_user_id',
       admissionProposalId: 'any_admission_proposal_id',
@@ -17,6 +26,21 @@ describe('DeclineAdmissionProposalController', () => {
     expect(declineAdmissionProposalSpy.perform).toHaveBeenCalledWith({
       user: { id: 'any_user_id' },
       admissionProposal: { id: 'any_admission_proposal_id' },
+    })
+  })
+
+  it('should return 404 if DeclineAdmissionProposal returns UserNotFoundError', async () => {
+    const expectedError = new UserNotFoundError('any_user_id')
+    declineAdmissionProposalSpy.perform.mockResolvedValueOnce(expectedError)
+
+    const httpResponse = await sut.handle({
+      userId: 'any_user_id',
+      admissionProposalId: 'any_admission_proposal_id',
+    })
+
+    expect(httpResponse).toEqual({
+      statusCode: 404,
+      data: [expectedError],
     })
   })
 })
