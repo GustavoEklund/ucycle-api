479e62d185b585b4b6e93beaa49dc6a3bacdea7d
Gustavo Eklund <gustavo04616@gmail.com>
2022-09-16 15:11:10 -0300

    feat: decorate SignUpController with Log and DbTransaction

--- src/main/factories/application/controllers/users/signup.ts
+++ src/main/factories/application/controllers/users/signup.ts
@@ -1,6 +1,10 @@
 import { Controller, SignUpController } from '@/application/controllers'
 import { makeSignUpUseCase } from '@/main/factories/domain/use-cases/users'
+import { makeLogErrorControllerDecorator } from '@/main/factories/application/decorators/log-error-controller'
+import { makePgTransactionControllerDecorator } from '@/main/factories/application/decorators'
 
 export const makeSignUpController = (): Controller => {
-  return new SignUpController(makeSignUpUseCase())
+  const controller = new SignUpController(makeSignUpUseCase())
+  const dbTransactionControllerDecorator = makePgTransactionControllerDecorator(controller)
+  return makeLogErrorControllerDecorator(dbTransactionControllerDecorator)
 }
